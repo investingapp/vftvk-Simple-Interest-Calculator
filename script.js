9bb3476c314ce43eb2ca8ec774a7ce310c83adbf
Kwasi Foh-Asare <kayfoh@gmail.com>
2021-07-09 01:56:19 +0000

    changes made from lab

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-
+    
     var principal = document.getElementById("principal").value;
 
     var rate = document.getElementById("rate").value
